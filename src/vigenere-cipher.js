308b126b94d6c7a813206edecd36ea8ee21143fb
skaletta95 <windows.adam0701@gmail.com>
2025-01-02 22:37:51 +0300

    feat: can`t fo last 2 tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
+  encrypt(message, key) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
-  decrypt() {
+    }
+
+  decrypt(message, key) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  // remove line with error and write your code here
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined || !(date instanceof Date) || isNaN(date)) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getMonth();
+
+  if (month === 11 || month < 2) {
+    return "winter";
+  } else if (month < 5) {
+    return "spring";
+  } else if (month < 8) {
+    return "summer";
+  } else {
+    return "fall";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
